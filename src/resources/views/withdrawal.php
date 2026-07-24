3e01c417789a27dd41f3901bc540a43467cb7b66
youhonami <youhonami42@gmail.com>
2026-07-24 12:24:56 +0900

    退会ページのデザイン

--- src/resources/views/withdrawal.php
+++ src/resources/views/withdrawal.php
@@ -6,43 +6,51 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>退会</title>
+    <link rel="stylesheet" href="<?= asset('css/withdrawal.css') ?>">
 </head>
 
 <body>
-    <h1>退会ページ</h1>
-    <p>退会すると、これまでに記入した日記もすべて削除されます。</p>
+    <main class="withdrawal-page">
+        <section class="withdrawal-card">
+            <div class="withdrawal-heading">
+                <p class="withdrawal-subtitle">Diary</p>
+                <h1>退会ページ</h1>
+                <p>退会すると、これまでに記入した日記もすべて削除されます。</p>
+            </div>
 
-    <?php if ($errors->any()): ?>
-        <ul>
-            <?php foreach ($errors->all() as $error): ?>
-                <li><?= e($error) ?></li>
-            <?php endforeach; ?>
-        </ul>
-    <?php endif; ?>
+            <?php if ($errors->any()): ?>
+                <ul class="message message-error">
+                    <?php foreach ($errors->all() as $error): ?>
+                        <li><?= e($error) ?></li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php endif; ?>
 
-    <?php if (session('withdrawal_error')): ?>
-        <p><?= e(session('withdrawal_error')) ?></p>
-    <?php endif; ?>
+            <?php if (session('withdrawal_error')): ?>
+                <p class="message message-error"><?= e(session('withdrawal_error')) ?></p>
+            <?php endif; ?>
 
-    <form action="<?= route('withdrawal.destroy') ?>" method="post">
-        <?= csrf_field() ?>
+            <form action="<?= route('withdrawal.destroy') ?>" method="post" class="withdrawal-form">
+                <?= csrf_field() ?>
 
-        <div>
-            <label for="email">メールアドレス</label>
-            <input type="email" id="email" name="email" value="<?= e(old('email')) ?>" required>
-        </div>
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input type="email" id="email" name="email" value="<?= e(old('email')) ?>" placeholder="example@example.com" required>
+                </div>
 
-        <div>
-            <label for="password">パスワード</label>
-            <input type="password" id="password" name="password" minlength="6" required>
-        </div>
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input type="password" id="password" name="password" minlength="6" placeholder="6文字以上" required>
+                </div>
 
-        <button type="submit">退会する</button>
-    </form>
+                <button type="submit" class="withdrawal-button">退会する</button>
+            </form>
 
-    <p>
-        <a href="<?= route('login.index') ?>">ログインページへ戻る</a>
-    </p>
+            <p class="back-link">
+                <a href="<?= route('login.index') ?>">ログインページへ戻る</a>
+            </p>
+        </section>
+    </main>
 </body>
 
 </html>
